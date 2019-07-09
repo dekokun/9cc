32aa9ca53770a7bbbe0fe5c5c6bbf6e7bd302bba
dekokun <shintaro.kurachi@gmail.com>
2019-07-09 09:52:32 +0900

    unneed placeholder

--- 9cc.c
+++ 9cc.c
@@ -17,7 +17,7 @@ void error_at(char *loc, char *fmt, ...) {
   int pos = loc - user_input;
   fprintf(stderr, "%s\n", user_input);
   fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
-  fprintf(stderr, "^ %s");
+  fprintf(stderr, "^");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
